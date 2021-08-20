868a4e3835314e3bf510ea0f42a69aa28891e96c
Unbekannt <39250953+hernanbuck@users.noreply.github.com>
2021-08-20 14:09:24 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -14,3 +14,11 @@ docker-compose ps
 echo "==> Enable Docker Service"
 systemctl enable docker
 
+echo "==> Install Telegraf"
+sudo dpkg -i telegraf_1.19.3-1_amd64.deb
+
+echo "==> Dejo corriendo envio de datos en 2do plano"
+telegraf --config /home/docker-compose-influxdb-grafana/telegraf.conf > /dev/null 2>&1 &
+
+echo "RECORDAR GUARDAR EN CRON LA SIGUIENTE LINEA @reboot        telegraf --config /home/docker-compose-influxdb-grafana/telegraf.conf > /dev/null 2>&1 &"
+
